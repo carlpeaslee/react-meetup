11dd616e12b026264deabcad8eaf7038a33c88c9
Carl Peaslee <carlpeaslee@Carls-MacBook-Pro.local>
2017-04-18 09:32:56 -0500

    set up auth0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
+import Auth0 from 'auth0-lock'
+import {auth0Domain, auth0ClientId, url} from 'config'
 
 class App extends Component {
+
+  state = {
+    email: false
+  }
+
+  constructor() {
+    super()
+    this.auth = new Auth0(
+      auth0ClientId,
+      auth0Domain,
+      {
+        auth: {
+          redirectUrl: url,
+          responseType: 'token',
+          params: {
+            scope: 'openid email'
+          }
+        }
+      }
+    )
+    this.auth.on('authenticated', (authResponse) => {
+      this.setState(
+        (prevState, props) => {
+          let {email} = authResponse.idTokenPayload
+          return {
+            email
+          }
+        },
+        () => {
+          //a callback
+        }
+      )
+    })
+  }
+
   render() {
     return (
-      <h2>
-        Hello React Meetup
-      </h2>
+      <div>
+        {(this.state.email) ?
+          <h3>
+            Hello {this.state.email}
+          </h3> :
+          <button
+            onClick={()=>{
+              this.auth.show()
+            }}
+          >
+            Login!
+          </button>
+        }
+
+      </div>
     )
   }
 }
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const auth0Domain = 'carlpeaslee.auth0.com'
+export const auth0ClientId = 'zVSZiYLG3ubW9qV2k2WqJusgOANxXJbC'
+
+export const url = (process.env.NODE_ENV === 'production') ? 'https://react-meetup.herokuapp.com/' : 'http://localhost:3000'
